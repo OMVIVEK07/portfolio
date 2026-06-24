4d1f576cf9fc2482b6da1dcd133850e1903ce079
OMVIVEK07 <omvivek0506@gmail.com>
2026-06-24 14:01:17 +0530

    fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -136,7 +136,7 @@ const Hero = () => {
             <div className="relative">
               <div className="w-64 h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 rounded-full overflow-hidden border-4 border-white dark:border-gray-700 shadow-2xl animate-float">
                 <img
-                  src="/image.jpeg"
+                  src="image.jpeg"
                   alt="OM VIVEK MALLIKANTI"
                   className="w-full h-full object-cover"
                 />
